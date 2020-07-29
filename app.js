e8b5f33631582be7d3362f193f54de68bb19e6b4
Aaron You <yhaoquan@gmail.com>
2020-07-28 23:49:57 -0400

    please change image signature

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ var dboptions = {
 let mongodb_connection_string = 'mongodb://mongo:27017/' + "my-grading-space";
 
 if(process.env.MONGODB_SERVICE_HOST){
+    
     mongodb_connection_string = "mongodb://"+process.env.MONGODB_SERVICE_HOST +':'+process.env.MONGODB_SERVICE_PORT_MONGO+ "/my-grading-space";
   
   }
